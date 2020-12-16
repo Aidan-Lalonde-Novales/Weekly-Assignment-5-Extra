862536ceb92b57ac879b89d65d03502f73f64366
Aidan Lalonde-Novales <aidan.lalonde-novales@ocsbstudent.ca>
2020-12-16 20:31:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Extra Python Program that calculates the Area of a Circle
+
+PI = 3.14
+radius = float(input(' Please Enter the radius of a circle: '))
+area = PI * radius * radius
+circumference = 2 * PI * radius
+
+print(" Area Of a Circle = %.2f" %area)
